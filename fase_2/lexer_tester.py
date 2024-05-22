207bea114b29e36710fb0cd391f6ebf34976175d
Fabio Miranda <fabiomiranda0421@gmail.com>
2024-05-22 12:07:30 +0100

    correção do exemplo

--- fase_2/lexer_tester.py
+++ fase_2/lexer_tester.py
@@ -5,9 +5,9 @@ FUNCAO somatorio( x:xs ),: x + somatorio(xs) ;
 resultado = somatorio([1,2,3]);
 """,
 """
-lista1 = map( mais2, [] ); -- []
+lista1 = map( mais2, [] ); − []
 lista2 = map( mais2, [ 1, 2, 3 ] );
-−−  [ mais2(1),mais2(2),mais2(3)] = [3,4,5]
+− [ mais2(1),mais2(2),mais2(3)] = [3,4,5]
 lista3 = fold( soma, [ 1, 2, 3 ], 0 );
 \{−  exemplo interpolacao de strings
 Ola, EST IPCA!−\}
